39fa16f7be1eba187413ea899cfa1c7431bbcb1b
naufalpens <naufalpens@gmail.com>
2022-06-03 14:30:37 +0700

    update search BP

--- views/bpartner/_search.php
+++ views/bpartner/_search.php
@@ -21,20 +21,27 @@ use yii\widgets\ActiveForm;
 
     <div class="row clearfix">
         <div class="col-sm-12 col-md-2">
+            <?= $form->field($model, 'value') ?>
+        </div>
+        <div class="col-sm-12 col-md-2">
             <?= $form->field($model, 'first_name') ?>
         </div>
-        <div class="col-sm-12 col-md-2">
-            <?= $form->field($model, 'last_name') ?>
-        </div>
-        <div class="col-sm-12 col-md-2">
-            <?= $form->field($model, 'birth_date')->widget(\kartik\date\DatePicker::class, [
-                'removeButton' => false,
-                'pluginOptions' => [
-                    'autoclose' => true,
-                    'format' => 'yyyy-mm-dd'
-                ]
-            ]) ?>
-        </div>
+        <?php if($model->type == 'employee') { ?>
+            <div class="col-sm-12 col-md-2">
+                <?= $form->field($model, 'last_name') ?>
+            </div>
+
+            <div class="col-sm-12 col-md-2">
+                <?= $form->field($model, 'birth_date')->widget(\kartik\date\DatePicker::class, [
+                    'removeButton' => false,
+                    'pluginOptions' => [
+                        'autoclose' => true,
+                        'format' => 'yyyy-mm-dd'
+                    ]
+                ]) ?>
+            </div>
+        <?php } ?>
+        
         <div class="col-sm-12 col-md-2">
             <?= $form->field($model, 'status')->dropDownList(ListComponent::getListDataStatus(), [
                 'prompt' => 'SEMUA'
